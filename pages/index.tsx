bab38b75dcd1bff0a75d80f9120b95a20d475208
Atul Kumar <68207869+atuljaiss@users.noreply.github.com>
2022-03-31 23:13:17 +0530

    day-2 modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Typewriter from 'typewriter-effect'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="flex h-screen flex-col bg-gradient-to-br from-cyan-400 to-purple-600 ">
       <Head>
         <title>NFT DROP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className='text-red-500 text-4xl'>Welcome</h1>
-
+      <h1 className='text-8xl font-bold text-center justify-center
+                 text-BLACK mt-20 p-10'>NFT DROP</h1>
+             <h2 className='text-5xl text-black text-center'>
+             <Typewriter
+                    onInit={(typewriter) => {
+                    typewriter.typeString(
+                     "Welcome to NFT DROP challenge " 
+                    )
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString(
+                    "Click below link to see my build"
+                    )
+                    .pauseFor(2000)
+                    .pause()
+                    .start();
+                    }}
+                />
+                </h2>
+                <a className='text-4xl text-center p-10 text-white' 
+                href="https://aj-nftdrop.vercel.app/nft/atul">My Day-2 Work(click me)</a>
+      
       
     </div>
   )
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import TypeWriter from 'react-typewriter'
+import Typewriter from 'typewriter-effect';
 function NFTDropPage() {
 
     //nft auth
@@ -12,12 +14,12 @@ function NFTDropPage() {
         lg:grid lg:grid-cols-10'>
         {/* left */}
         <div className='bg-gradient-to-br lg:col-span-4
-        from-cyan-800 to-rose-500'>
+        from-cyan-400 to-purple-600'>
           <div className=' flex flex-col 
             items-center justify-center
             py-2 lg:min-h-screen '>
               <div className=' bg-gradient-to-br rounded-xl
-              from-yellow-400 to-purple-600 '>
+              from-blue-800 to-pink-400 '>
                 <img className='w-44 rounded-xl p-2
                     object-cover lg:h-96 lg:w-72'
                     src='https://links.papareact.com/8sg' 
@@ -26,9 +28,32 @@ function NFTDropPage() {
               </div>
              
              <div className=' p-5 text-center space-y-2 '>
-                 <h1 className='text-4xl font-bold
-                 text-white '>ATULJAISS Apes</h1>
-                 <h2 className='text-xl text-gray-300'>A collection of Atul jaiss Apes who live & breathe react</h2>
+             <h1 className='text-4xl font-bold
+                 text-white '>Atul-Jaiss Apes</h1>
+             <h2 className='text-xl text-gray-300'>
+             <Typewriter
+                    onInit={(typewriter) => {
+                    typewriter.typeString(
+                     "A collection of Atul jaiss Apes " 
+                    )
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString(
+                    "who live"
+                    )
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("And breathe react")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("A collection of Atul jaiss Apes who live and breathe react")
+                    .pause()
+                    .start();
+                    }}
+                />
+                </h2>
+             
+                
              </div>
 
           </div>
@@ -41,33 +66,42 @@ function NFTDropPage() {
                     font-extralight sm:w-80 '>
                     The{' '} 
                     <span className='font-extrabold underline 
-                    decoration-pink-600/50'>
-                        ATULJAISS</span> {' '}
+                    decoration-purple-600/50'>
+                        Atul-Jaiss</span> {' '}
                     NFT Market Place
                 </h1>
                 <button onClick={()=>{
                     address? disconnect():connectWithMetamask()
-                }} className='rounded-full bg-rose-400 px-4 py-2
+                }} className='rounded-full bg-purple-500 px-4 py-2
                  text-xs font-bold text-white lg:px-5 lg:py-3'>
                      {address?'Sign out':'Sign in'}</button>
             </header>
             <hr className='my-2 border' />
             {address && (
-                <p className='text-center text-sm text-rose-400'>You're login in with wallet {address.substring(0,5)}...{address.substring(address.length-5)}</p>
+                <p className='text-center text-sm text-green-600'>You're login in with wallet {address.substring(0,5)}...{address.substring(address.length-5)}</p>
             )}
             {/* content */}
             <div className='mt-10 flex flex-1 flex-col items-center 
             space-y-6 text-center lg:space-y-0 lg:justify-center'>
                 <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" 
                 alt="" ></img>
-                <h1 className='text-3xl font-bold lg:font-extrabold
-                lg:text-5xl'>The ATULJAISS Ape Coding Club | NFT Drop</h1>
+                <TypeWriter typing={0.5}>{<h1 className='text-3xl font-bold lg:font-extrabold
+                lg:text-5xl'> "The Atul-Jaiss Ape Coding Club | NFT Drop" </h1>
+                }</TypeWriter>
+                
+                   
+            
                 <p className='pt-2 text-xl text-green-500'>
                     13 / 21 NFT's claimed</p>
             </div>
             
+    <div>
+      
+      
+    </div>
+
             {/* Mint button*/}
-            <button className='h-16 bg-red-600 w-full text-white 
+            <button className='h-16 bg-purple-600 w-full text-white 
             font-bold rounded-full'>
                 Mint NFT (0.01 ETH)
             </button>
